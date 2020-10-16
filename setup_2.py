b0b28affa10961e1a323d4316f6336dd24b697aa
瀬尾　翼 <seotsubasa@MacBook-Miro.local>
2020-10-16 18:05:21 +0900

    master

--- setup_2.py
+++ setup_2.py
@@ -25,50 +25,52 @@ with open('data.csv', 'r') as f:
 
 if reader[0] == 1:               #1
    GPIO.output(15,GPIO.HIGH)
-else reader[0] == 0:
+elif reader[0] == 0:
    GPIO.output(15,GPIO.ROW)
 
 if reader[1] == 1:               #2
    GPIO.output(19,GPIO.HIGH)
-else reader[1] == 0:
+elif reader[1] == 0:
    GPIO.output(19,GPIO.ROW)
 
 if reader[2] == 1:               #3
    GPIO.output(21,GPIO.HIGH)
-else reader[2] == 0:
+elif reader[2] == 0:
    GPIO.output(21,GPIO.ROW)
 
 if reader[3] == 1:               #4
    GPIO.output(23,GPIO.HIGH)
-else reader[3] == 0:
+elif reader[3] == 0:
    GPIO.output(23,GPIO.ROW)
 
 if reader[4] == 1:               #5
    GPIO.output(27,GPIO.HIGH)
-else reader[4] == 0:
+elif reader[4] == 0:
    GPIO.output(27,GPIO.ROW)
 
 if reader[5] == 1:               #6
    GPIO.output(29,GPIO.HIGH)
-else reader[5] == 0:
+elif reader[5] == 0:
    GPIO.output(29,GPIO.ROW)
 
 if reader[6] == 1:               #7
    GPIO.output(31,GPIO.HIGH)
-else reader[6] == 0:
+elif reader[6] == 0:
    GPIO.output(31,GPIO.ROW)
 
 if reader[7] == 1:               #8
    GPIO.output(33,GPIO.HIGH)
-else reader[7] == 0:
+elif reader[7] == 0:
    GPIO.output(33,GPIO.ROW)
 
 if reader[8] == 1:               #9
    GPIO.output(35,GPIO.HIGH)
-else reader[8] == 0:
+elif reader[8] == 0:
    GPIO.output(35,GPIO.ROW)
 
 if reader[9] == 1:               #10
    GPIO.output(37,GPIO.HIGH)
-else reader[9] == 0:
+elif reader[9] == 0:
    GPIO.output(37,GPIO.ROW)
+
+GPIO.cleanup()
